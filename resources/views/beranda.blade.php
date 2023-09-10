4d32bec5fdb2bc28e668657e73bf4468d687d4f6
Irgo Satya <irgosg@gmail.com>
2023-09-11 00:07:55 +0700

    update hari ini

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Web</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="container max-w-full p-4 bg-sky-500">
+        <div class="flex">
+            <img src="/assets/logo.png" alt="Logo">
+        </div>
+        <div class="max-w-min mx-auto">
+            <ul class="text-xl font-semibold flex flex-row">
+                <li class=" mr-4">Home</li>
+                <li class=" mr-4">About</li>
+                <li class=" mr-4">Product</li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
